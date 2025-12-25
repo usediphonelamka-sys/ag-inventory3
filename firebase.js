99230ee782b679705ea72d816ff8d6a3cf4a871e
usediphonelamka-sys <usediphonelamka@gmail.com>
2025-12-25 23:55:16 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// ⭐ PASTE YOUR CONFIG HERE ⭐
 const firebaseConfig = {
   apiKey: "AIzaSyDsX6p_KzOG84L2tClqsXTzhyertGsJ9-U",
-  authDomain: "ag-inventory-4c089.firebaseapp.com",
-  projectId: "ag-inventory-4c089",
-  storageBucket: "ag-inventory-4c089.firebasestorage.app",
-  messagingSenderId: "1040708274288",
-  appId: "1:1040708274288:web:60841d932ac60e6ba94cb5",
-  measurementId: "G-KVXV91C16H"
+  authDomain: "ag-inventory-4c089.firebaseapp.com",
+  projectId: "ag-inventory-4c089",
+  storageBucket: "ag-inventory-4c089.appspot.com",
+  messagingSenderId: "1040708274288",
+  appId: "1:1040708274288:web:60841d932ac60e6ba94cb5"
 };
 
 const app = initializeApp(firebaseConfig);
